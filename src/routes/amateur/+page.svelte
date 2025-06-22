d7e6e80866783964deaa1ee1e63712ea6f644c7d
unknown <jdelrosario2121@gmail.com>
2025-06-22 19:03:26 +0800

    amateur landing page

---
+++ src/routes/amateur/+page.svelte
@@ -0,0 +1,360 @@
+<script>
+	const events = [
+		{
+			image:
+				'https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67f32d560a5eef242ae8e8dc_hand-holding-circle-dollar-thin.svg',
+			alt: 'image',
+			desctiption: 'Lower Coast'
+		},
+		{
+			image:
+				'https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67f32f4755ccc1914c8aae51_coins-thin.svg',
+			alt: 'image',
+			desctiption: 'New revenue'
+		},
+		{
+			image:
+				'https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67f32db2e8c17343f96f450b_puzzle-thin.svg',
+			alt: 'image',
+			desctiption: 'Better matchups'
+		},
+		{
+			image:
+				'https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67f32e660a5eef242ae96aed_people-line-thin.svg',
+			alt: 'image',
+			desctiption: 'More participation'
+		},
+		{
+			image:
+				'https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67f32e8fb1c8a7c55b86df59_calendar-star-thin.svg',
+			alt: 'image',
+			desctiption: 'More events'
+		},
+		{
+			image:
+				'https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67ee5b2aff0ec372cf1ab112_chart-mixed-up-circle-dollar-thin.svg',
+			alt: 'image',
+			desctiption: 'Bigger profits'
+		}
+	];
+
+	const success = [
+		{
+			image:
+				'https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67f33e9c829f7ceee404395d_85888df7f79bb940acfa45874da9256b_fans-cheering-light-p-1080.png',
+			alt: 'success image',
+			title: 'Increase Event Participation',
+			description:
+				'AI-powered scheduling matches teams evenly, maintaining high competition levels and increasing event appeal.'
+		},
+		{
+			image:
+				'https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67f341172a268a4d884319e7_72bd369b24425b13365c94863b1b788b_fans-on-phones-light-p-1080.png',
+			alt: 'success image',
+			title: 'Next-level Experience',
+			description:
+				'Engage teams and fans with real-time scores, rankings, and professional event communication tools.'
+		},
+		{
+			image:
+				'https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67f3431cee5ea212f7767fbd_217cfdc2852cfe517a0bcd2bced215a7_revenue-growth-light-p-1080.png',
+			alt: 'success image',
+			title: 'Revenue Growth',
+			description:
+				'Tap into sponsorship networks for experiential activations, branded promotions, and increased ticket sales.'
+		},
+		{
+			image:
+				'https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67f344f35d32341bec60d775_integrated-workflow-light-p-1080.png',
+			alt: 'success image',
+			title: 'Seamless Operations',
+			description:
+				'Integrated registration, payment processing, CRM, and mobile event management eliminate tedious tasks.'
+		}
+	];
+
+	const user = [
+		{ userNumber: '1500+', name: 'Events powered annually' },
+		{ userNumber: '50%', name: 'Boost in sponsorship revenue' },
+		{ userNumber: '100,000+', name: 'Teams registered' },
+		{ userNumber: '150,000+', name: 'Games scheduled' },
+		{ userNumber: '2,500,000+', name: 'Competitors' }
+	];
+
+	const app = [
+		{
+			image:
+				'https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67f3a098c425662ee1a9e5e9_grid-2-plus-thin.svg',
+			alt: 'register',
+			title: 'Easy Registrations',
+			description:
+				'Integrated e-commerce for quick team sign-ups, reducing admin tasks and boosting participation.'
+		},
+		{
+			image:
+				'https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67ee5b2aff0ec372cf1ab112_chart-mixed-up-circle-dollar-thin.svg',
+			alt: 'money',
+			title: 'Revenue Optimization',
+			description:
+				'Built-in analytics uncover insights, identify trends, and help maximize your revenue opportunities.'
+		},
+		{
+			image:
+				'https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67f32db2e8c17343f96f450b_puzzle-thin.svg',
+			alt: 'fair',
+			title: 'Fair Matchups',
+			description:
+				'AI-powered scheduling with RPI rankings ensures balanced, competitive play every time.'
+		},
+		{
+			image:
+				'https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67f32e660a5eef242ae96aed_people-line-thin.svg',
+			alt: 'enhance',
+			title: 'Enhanced Engagement',
+			description:
+				'Pro-level mobile app delivering real-time scores, schedules, and rankings for teams and fans.'
+		}
+	];
+</script>
+
+<div class="flex h-full w-full items-center justify-center bg-white text-black">
+	<div class="w-full">
+		<div class="flex flex-col items-center gap-10 overflow-hidden font-[unset]">
+			<div
+				class="grid h-96 w-screen bg-cover bg-center bg-no-repeat max-lg:place-items-center xl:h-screen xl:grid-cols-2"
+				style="background-image: url('https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67f325629916bdf9ef9feac6_compete_hero.png');"
+			>
+				<section
+					class="mx-auto flex flex-col justify-center gap-5 p-4 max-xl:h-96 max-xl:items-center xl:p-14 max-md:text-center"
+				>
+					<h2
+						class="leading-tight font-black tracking-tighter max-sm:text-lg sm:text-2xl md:text-3xl lg:text-4xl xl:text-7xl"
+					>
+						The Ultimate <strong class="text-primary">AI-Powered</strong><br />
+						Sports Operations Engine
+					</h2>
+					<p class="font-semibold lg:text-lg">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa facere, consectetur vero
+						unde esse dignissimos harum, impedit debitis dolor at eos quasi odio illum officiis.
+						Neque expedita quae sequi nisi.
+					</p>
+					<div>
+						<button class="btn btn-primary group flex items-center justify-start gap-2 text-center">
+							Choose Your Role
+							<span
+								class="transform text-2xl transition-all duration-300 ease-in-out group-hover:translate-x-2"
+							>
+								&rarr;
+							</span>
+						</button>
+					</div>
+				</section>
+				<section></section>
+			</div>
+		</div>
+		<div class="flex justify-center px-14 py-6">
+			<div class="mt-10 flex flex-col gap-4">
+				<div class="flex h-80 items-center justify-center">
+					<h4 class="font text-center text-2xl font-semibold">
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit ipsa dolore quo id ipsam <br
+						/>
+						odio exercitationem perspiciatis. Delectus dolorem eum voluptatum at maxime saepe, ex, atque
+						earum ipsa sapiente facilis.
+					</h4>
+				</div>
+
+				<div class="border-neutral rounded-xl border-2">
+					<div
+						class="relative grid overflow-hidden rounded-xl border bg-black text-white lg:grid-cols-2"
+					>
+						<div
+							class="absolute inset-0 bg-cover bg-no-repeat opacity-30"
+							style="background-image: url('https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67cf90467d40b125f28478e4_wave-1.webp');"
+						></div>
+
+						<div class="relative z-10">
+							<img
+								src="https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67f32c983f990d0c170de735_volleyball-hands-square.webp"
+								alt="handimage"
+							/>
+						</div>
+
+						<div class="relative z-10 flex flex-col gap-8 py-10 max-lg:text-center">
+							<h2 class="text-3xl font-bold tracking-tighter lg:text-6xl">
+								Built for Sports Operators Who Want to <strong class="text-primary">Grow</strong>
+							</h2>
+							<p class="max-w-xl text-xl font-semibold text-balance">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit illo consequuntur
+								maiores...
+							</p>
+
+							<div class="flex justify-center lg:justify-start">
+								<div class="mt-10 grid sm:grid-cols-2 lg:grid-cols-3 max-lg:gap-10">
+									{#each events as { image, alt, desctiption }}
+										<div class="mb-4 text-center">
+											<img
+												src={image}
+												{alt}
+												class="mx-auto size-16"
+												style="filter: brightness(0) invert(1)"
+											/>
+											<p class="text-xl font-bold">{desctiption}</p>
+										</div>
+									{/each}
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="md:grid md:grid-cols-3 pb-4">
+						<div class="flex flex-col justify-between px-4">
+							<div class="max-md:text-center ">
+								<h6 class="my-6 text-3xl font-bold">
+									Powerful Features Designed for <strong class="text-primary">Success</strong>
+								</h6>
+								<p class="text-neutral md:max-w-lg text-lg font-semibold text-balance">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolorum architecto
+									odit, delectus quo ut nostrum dolore eveniet rerum officia ab repellat aspernatur
+									illo, enim quisquam asperiores quam pariatur. Voluptate.
+								</p>
+							</div>
+							<button class="btn btn-neutral max-md:mb-4 max-md:my-6">GET STARTED FOR FREE</button>
+						</div>
+
+						<div class="col-span-2 grid grid-cols-2">
+							{#each success as { image, alt, title, description }}
+								<div class="border-t-neutral flex flex-col gap-4 border p-4 max-md:text-center">
+									<img src={image} {alt} class="opacity-40" />
+									<h6 class="md:mt-16 text-2xl font-black tracking-tight">{title}</h6>
+									<p class="max-w-sm">{description}</p>
+								</div>
+							{/each}
+						</div>
+					</div>
+
+					<div class="bg-black text-white">
+						<div class="grid grid-cols-2 p-4">
+							<div class="flex max-w-xl flex-col gap-6 py-30">
+								<h6 class="font-bold text-balance text-2xl lg:text-4xl xl:text-7xl">
+									Compete Simplifies <strong class="text-secondary">Every Step</strong> of Event Management
+								</h6>
+								<p class="max-w-md lg:text-xl font-semibold text-balance">
+									Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates doloribus
+									delectus eveniet, eum laboriosam possimus reprehenderit velit, quisquam dolore,
+									est nemo perspiciatis pariatur perferendis officia ea itaque officiis iusto
+									impedit?
+								</p>
+
+								<p>Get the Fastbreak advantage</p>
+								<button class="btn btn-secondary mt-6 w-40">SEE A DEMO</button>
+							</div>
+							<div>
+								<img
+									src="https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67e6654622b5c5c61ea59a22_pexels-pixabay-159607.webp"
+									alt="girl basketball"
+								/>
+							</div>
+						</div>
+						<div class="mt-20 text-center">
+							<h6 class="text-6xl font-black tracking-tight">Trusted by Leading Event Operators</h6>
+							<p class="mt-10 text-xl font-bold">How Do We Know?</p>
+						</div>
+						<div
+							class=" flex justify-center bg-cover bg-no-repeat py-30"
+							style="background-image: url(https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67cf9046ac6c6d99e011660b_wave-2-p-1080.webp);"
+						>
+							<div class="bg-neutral flex flex-col gap-10 rounded-2xl px-20 py-10">
+								{#each user as { userNumber, name }}
+									<div class="">
+										<h6 class="text-4xl font-bold">{userNumber}</h6>
+										<p class="mt-1 text-lg font-semibold tracking-tight">{name}</p>
+									</div>
+								{/each}
+							</div>
+						</div>
+
+						<div class="relative z-10 grid place-items-center bg-white p-10 text-center text-black">
+							<img
+								src="https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67b6a9477efb8337b00f0f58_erervfd.avif"
+								alt="avif"
+								class="size-10"
+								style="filter: brightness(100) invert(100%)"
+							/>
+							<h5 class="my-4 max-w-3xl font-black sm:text-2xl">
+								Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint cupiditate laboriosam
+								velit culpa nulla quod, inventore, vel reiciendis consectetur, mollitia quisquam.
+								Facilis distinctio similique voluptatibus perspiciatis officiis illo, facere at!ped,
+								fine-tuned and refined with the power of Fastbreak AI - an industry leader.
+							</h5>
+							<h6 class="text-lg font-bold">Joshua Del Rosario</h6>
+							<p class="mb-4 text-lg font-bold">Ridgewood Rebels Feeder</p>
+							<img
+								src="https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67f964a3bf5b31599f62105c_download.jpg"
+								alt="conference logo"
+								class="mt-4 w-30"
+							/>
+						</div>
+
+						<div class="flex flex-col gap-16 bg-white p-6 text-black">
+							<div>
+								<h6 class="sm:text-5xl max-lg:text-center lg:text-7xl font-black tracking-tighter">
+									Finally, A <strong class="text-primary ">Complete</strong> <br />Sports Operations
+									Center
+								</h6>
+								<p class="mt-10 max-w-xl text-lg font-semibold max-lg:text-center">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur blanditiis,
+									temporibus quos accusantium error hic similique quibusdam autem doloribus est,
+									voluptatibus ullam animi corporis inventore beatae, obcaecati nemo vitae officia!
+								</p>
+							</div>
+							<div class="grid lg:grid-cols-2">
+								<div class="grid grid-cols-3 gap-5">
+									{#each app as { image, alt, title, description }}
+										<div class="flex flex-col gap-2">
+											<img src={image} {alt} class="size-14" />
+											<h6 class="text-neutral text-lg font-bold">{title}</h6>
+											<p class="text-neutral text-sm">{description}</p>
+										</div>
+									{/each}
+								</div>
+
+								<div>
+									<img
+										src="https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67da21ea5e95c4af44a5eb75_assets-image-18.png"
+										alt="gadget"
+										class=""
+									/>
+								</div>
+							</div>
+						</div>
+
+						<div class="grid bg-black xl:grid-cols-3">
+							<div class="flex flex-col gap-10 px-6 py-20 max-lg:text-center">
+								<h5 class="sm:text-5xl lg:text-7xl font-black tracking-tight">
+									Run Smarter, More Profitable Sports Events <strong class="text-primary"
+										>Today</strong
+									>
+								</h5>
+								<p class="lg:text-xl font-light">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus aut necessitatibus
+									mollitia voluptates officiis impedit quaerat dolor quibusdam ab obcaecati modi ex
+									commodi maxime asperiores consequuntur, molestias ducimus molestiae deleniti?
+								</p>
+								<div class="flex max-lg:justify-center gap-6">
+									<button class="btn btn-primary">SIGN UP NOW</button>
+									<button class="btn btn-ghost">GET A DEMO</button>
+								</div>
+							</div>
+							<div class="col-span-2 grid">
+								<img
+									src="https://cdn.prod.website-files.com/67b6a9467efb8337b00f0d4c/67ee7230387e58264aabb72c_volleyball.webp"
+									alt="volleyball"
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
